c68fc122101c68a02a5dcf2a5917ebc6ee71fd31
kavan25 <kavanthakar1996@gmail.com>
2020-11-09 13:26:37 +0530

    Updates

---
+++ precache-manifest.81c65abc2ae7e81c46618d55ff26facc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "777c4b98bfb80a118c33adac80698c66",
+    "url": "/ReactJS_Todo_List/index.html"
+  },
+  {
+    "revision": "b43df14cb61175c33487",
+    "url": "/ReactJS_Todo_List/static/css/main.de343097.chunk.css"
+  },
+  {
+    "revision": "72032899d13683aca7ad",
+    "url": "/ReactJS_Todo_List/static/js/2.fa9bdf05.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/ReactJS_Todo_List/static/js/2.fa9bdf05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b43df14cb61175c33487",
+    "url": "/ReactJS_Todo_List/static/js/main.646cddb9.chunk.js"
+  },
+  {
+    "revision": "5892bdc451e8c8f2505b",
+    "url": "/ReactJS_Todo_List/static/js/runtime-main.2efb0f1a.js"
+  }
+]);
